5c327ee707c581ad8b559713414ee3c09a8c711e
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-26 19:54:48 -0600

    ci: Add Moods

--- src/components/ui/meditation/Timer.tsx
+++ src/components/ui/meditation/Timer.tsx
@@ -1,5 +1,28 @@
-import {StyleSheet, Text, TouchableOpacity, View, Image} from "react-native";
-import {COLORS} from "@constants";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
+import { COLORS } from "@constants";
+
+const MOOD = [
+    {
+        id: 1,
+        name: 'Todos',
+    },
+    {
+        id: 2,
+        name: 'Dormir',
+    },
+    {
+        id: 3,
+        name: 'Relajarse'
+    },
+    {
+        id: 4,
+        name: 'Feliz'
+    },
+    {
+        id: 5,
+        name: 'Cansado'
+    }
+];
 
 export const Timer = () => {
     return (
@@ -11,23 +34,51 @@ export const Timer = () => {
 
             <View style={styles.playContainer}>
                 <TouchableOpacity style={styles.playButton}>
-                    <Image source ={require('@images/play.webp') }/>
+                    <Image source={require('@images/play.webp')} />
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.playButton}>
-                    <Image source ={require('@images/stop.webp') }/>
+                    <Image source={require('@images/stop.webp')} />
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.playButton}>
-                    <Image source ={require('@images/reset.webp') }/>
+                    <Image source={require('@images/reset.webp')} />
                 </TouchableOpacity>
             </View>
-
+            <Text style={{color: '#fff', marginTop: 15}}>Selecciona un estado de amimo para meditar</Text>
+            <View style={styles.mood} >
+                {
+                    MOOD.map((mood) => {
+                        return (
+                            <TouchableOpacity style={styles.moodButton} key={mood.id}>
+                                <Text style={styles.moodText}>{mood.name}</Text>
+                            </TouchableOpacity>
+                        )
+                    })
+                }
+            </View>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center', // Aligning items in the center horizontally
+    },
+    mood: {
+      flexDirection: 'row',
+        gap: 20,
+        marginTop: 20,
+    },
+   moodButton: {
+       backgroundColor:  COLORS.backgroundSecundary,
+        padding: 10,
+        borderRadius: 30,
+        height: 80,
+        justifyContent: 'center',
+        maxWidth: 65,
+   },
+    moodText:{
+      color: '#fff',
+        fontSize: 10
     },
     timer: {
         backgroundColor: COLORS.dark,
@@ -37,13 +88,12 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    touchable: {
-        // Add styles for TouchableOpacity if needed
-    },
     playContainer: {
         marginTop: 30,
         flexDirection: 'row',
-        gap: 10
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        gap: 20
     },
     playButton: {
         justifyContent: 'center',
