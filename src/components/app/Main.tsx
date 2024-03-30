bff9105fc944edb9a008c067538ec78ff3c09205
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-30 03:25:32 -0600

    feat: add routes in appbar

--- src/components/app/Main.tsx
+++ src/components/app/Main.tsx
@@ -1,14 +1,15 @@
-import { View, StyleSheet, Text } from 'react-native';
-import { Timer } from '@components';
+import { View, Text } from 'react-native';
+import { Timer, AppBar } from '@components';
 import { Navigate, Route, Routes } from 'react-router-native';
 
 export const Main = () => {
     return (
         <View>
+            <AppBar/>
             <Routes>
-                <Route path="/" element={<Text>Hola</Text>} />
-                <Route path="/meditation" element={<Text>Hola</Text>} />
-
+                <Route path="/" element={<Timer/>} />
+                <Route path="/meditation" element={<Text></Text>} />
+                <Route path="*" element={<Navigate to='/' />}/>
             </Routes>
         </View>
     );
--- src/components/ui/appbar/AppBar.tsx
+++ src/components/ui/appbar/AppBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import {View, Text, TouchableWithoutFeedback, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import { Link, useLocation } from 'react-router-native';
+import { COLORS } from '@constants'
 
 const AppBarTab = ({ children, to }: any) => {
     const { pathname } = useLocation();
@@ -8,9 +9,9 @@ const AppBarTab = ({ children, to }: any) => {
 
     return (
         <Link to={to} component={TouchableWithoutFeedback}>
-            <Text style={[styles.tabText, active && styles.activeTabText]}>
+            <TouchableOpacity>
                 {children}
-            </Text>
+            </TouchableOpacity>
         </Link>
     );
 };
@@ -18,26 +19,28 @@ const AppBarTab = ({ children, to }: any) => {
 export const AppBar = () => {
     return (
         <View style={styles.appBar}>
-            <AppBarTab to="/">Inicio</AppBarTab>
-            <AppBarTab to="/meditation">Meditación</AppBarTab>
+            <AppBarTab to="/"><Image source={ require('@images/home.webp')}/></AppBarTab>
+            <AppBarTab to="/meditation"><Image source={ require('@images/meditation.webp')}/></AppBarTab>
+            <AppBarTab to="/community"><Image source={ require('@images/community.webp')}/></AppBarTab>
+            <AppBarTab to="/resources"><Image source={ require('@images/file.webp')}/></AppBarTab>
+            <AppBarTab to="/libreria"><Image source={ require('@images/book.webp')}/></AppBarTab>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     appBar: {
-        backgroundColor: '#333',
-        height: 80,
+        backgroundColor: COLORS.mediumDark,
+        color: COLORS.white,
+        height:  80,
+        padding: 10,
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
         flexDirection: 'row',
         justifyContent: 'space-around',
         alignItems: 'center',
-    },
-    tabText: {
-        color: '#fff',
-        fontSize: 16,
-    },
-    activeTabText: {
-        fontWeight: 'bold', // Opcional: cambia el estilo del texto cuando está activo
+        flexWrap: 'wrap',
     },
 });
 
